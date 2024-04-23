be3b716ffeee6e9da426435b557af292bc88bb5e
DDavitaa <c20406272@mytudublin.ie>
2024-04-23 11:26:02 +0100

    done

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -9,10 +9,6 @@ extends Node3D
 @export var multiplier:float = 5
 
 var old_length = length
-var old_start_angle = start_angle
-var old_frequency = frequency
-var old_base_size = base_size
-var old_multiplier = multiplier
 
 
 var creatureHead_scene = load("res://behaviors/creature_head.tscn")
@@ -20,53 +16,40 @@ var creatureBody_scene = load("res://behaviors/creature_body.tscn")
 var length_distance:float = 2
 
 var cubes_array = []
+var theta = 1
 
 func _process(delta):
 	DebugDraw3D.draw_sphere(global_position,1,Color.HOT_PINK,2.0)
 	DebugDraw3D.draw_sphere(global_position,2,Color.SKY_BLUE,2.0)
 	
-	if length != old_length or start_angle != old_start_angle or frequency != old_frequency or base_size != old_base_size or multiplier != old_multiplier:
+	if length != old_length :
 		remove_child(get_node("creatureHead"))
 		remove_child(get_node("creatureBody"))
 		length_distance = 0.0
 		cubes_array = []
-		
 		old_length = length
-		old_start_angle = start_angle
-		old_frequency = frequency
-		old_base_size = base_size
-		old_multiplier = multiplier
 		
 		create_creature()
 		
-	
 	#print(lerp(sin(10),10.0,frequency))
 	
 	
 func create_creature():
-	var n = sin(frequency)
-	
 	
 	var creatureHead_instance = creatureHead_scene.instantiate()
 	var creatureBody_instance = creatureBody_scene.instantiate()
+	
+	creatureHead_instance.global_position.x = length_distance
+	length_distance += creatureHead_instance.size.x
 	cubes_array.append(creatureHead_instance)
 	
 	for i in length:
 		var cube_instance = CSGBox3D.new()
-		#cube_instance.position.x = length_distance
-		#length_distance += cube_instance.size.x
+		cube_instance.position.x = length_distance
+		length_distance += cube_instance.size.x
 		creatureBody_instance.add_child(cube_instance)
 		cubes_array.append(creatureBody_instance)
-	
-	for i in cubes_array:
-		cubes_array[i].size.x = lerp(frequency,5.0,sin(frequency))
 		
-	
-	creatureHead_instance.global_position.x = length_distance
-	length_distance += creatureHead_instance.size.x
-	
-
-	
 	
 	add_child(creatureHead_instance)
 	add_child(creatureBody_instance)
