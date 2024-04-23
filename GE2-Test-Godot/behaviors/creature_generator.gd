ba5220e0b38d05f6d08c9e7e4d7771264c2a1e5f
DDavitaa <c20406272@mytudublin.ie>
2024-04-23 09:38:37 +0100

    drew spheres

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -1,13 +1,27 @@
 @tool
 extends Node3D
 
+# length, frequency, start_angle, base_size, multiplier
+@export var length:int
+@export var start_angle:float
+@export var frequency:float
+@export var base_size:float
+@export var multiplier:float
+
 
 func _process(delta):
-	pass		
+	DebugDraw3D.draw_sphere(global_position,2,Color.HOT_PINK,2.0)
+	DebugDraw3D.draw_sphere(global_position,4,Color.SKY_BLUE,2.0)
+	
+func create_cube():
+	for i in length:
+		pass
 
 func _ready():
-	if not Engine.is_editor_hint():		
+	if not Engine.is_editor_hint():
 		pass
+	
+	
 	
 
 
